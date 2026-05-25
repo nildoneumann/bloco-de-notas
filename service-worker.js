661988dab5af14801b575088b0f4f84c763de67e
nildoneumann <leonildoneumann@gmail.com>
2026-05-25 08:04:54 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE = 'notas-pwa-v1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+
+  e.waitUntil(
+
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+
+  );
+
+});
+
+self.addEventListener('fetch', e => {
+
+  e.respondWith(
+
+    caches.match(e.request)
+      .then(response => response || fetch(e.request))
+
+  );
+
+});
